befb75eca54923b52f7ed1b9370552aa24407f41
Dan Lopez <86757951+TheThinkPotato@users.noreply.github.com>
2022-02-26 13:11:25 +1000

    Add project files.

---
+++ cbrom/Form1.Designer.cs
@@ -0,0 +1,217 @@
+﻿namespace cbrom
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.buttonAddBios = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.listBoxRoms = new System.Windows.Forms.ListBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.buttonMakeNew = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox3 = new System.Windows.Forms.RichTextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.buttonOpen = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 15;
+            this.listBox1.Location = new System.Drawing.Point(620, 34);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(347, 139);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.DoubleClick += new System.EventHandler(this.listBox1_DoubleClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(705, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(183, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Selected Microcode Roms to Add";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "NCPU_MicroCode";
+            this.openFileDialog1.InitialDirectory = ".\\";
+            // 
+            // buttonAddBios
+            // 
+            this.buttonAddBios.Location = new System.Drawing.Point(61, 48);
+            this.buttonAddBios.Name = "buttonAddBios";
+            this.buttonAddBios.Size = new System.Drawing.Size(125, 24);
+            this.buttonAddBios.TabIndex = 2;
+            this.buttonAddBios.Text = "Add Original Bios";
+            this.buttonAddBios.UseVisualStyleBackColor = true;
+            this.buttonAddBios.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(367, 16);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(130, 15);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Microcode Patch Roms";
+            // 
+            // listBoxRoms
+            // 
+            this.listBoxRoms.FormattingEnabled = true;
+            this.listBoxRoms.ItemHeight = 15;
+            this.listBoxRoms.Location = new System.Drawing.Point(247, 34);
+            this.listBoxRoms.Name = "listBoxRoms";
+            this.listBoxRoms.Size = new System.Drawing.Size(347, 139);
+            this.listBoxRoms.TabIndex = 3;
+            this.listBoxRoms.SelectedIndexChanged += new System.EventHandler(this.listBoxRoms_SelectedIndexChanged);
+            this.listBoxRoms.DoubleClick += new System.EventHandler(this.listBoxRoms_DoubleClick);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(13, 196);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(477, 456);
+            this.richTextBox1.TabIndex = 5;
+            this.richTextBox1.Text = "";
+            // 
+            // buttonMakeNew
+            // 
+            this.buttonMakeNew.Location = new System.Drawing.Point(61, 89);
+            this.buttonMakeNew.Name = "buttonMakeNew";
+            this.buttonMakeNew.Size = new System.Drawing.Size(125, 24);
+            this.buttonMakeNew.TabIndex = 8;
+            this.buttonMakeNew.Text = "Make New Bios";
+            this.buttonMakeNew.UseVisualStyleBackColor = true;
+            this.buttonMakeNew.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(169, 178);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(175, 15);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Orignal Bios Microcode Patches";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(652, 178);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(160, 15);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "New Bios Microcode Patches";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(496, 196);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(477, 456);
+            this.richTextBox2.TabIndex = 10;
+            this.richTextBox2.Text = "";
+            // 
+            // richTextBox3
+            // 
+            this.richTextBox3.Location = new System.Drawing.Point(13, 675);
+            this.richTextBox3.Name = "richTextBox3";
+            this.richTextBox3.Size = new System.Drawing.Size(960, 159);
+            this.richTextBox3.TabIndex = 12;
+            this.richTextBox3.Text = "";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(13, 658);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(27, 15);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Log";
+            // 
+            // buttonOpen
+            // 
+            this.buttonOpen.Location = new System.Drawing.Point(61, 132);
+            this.buttonOpen.Name = "buttonOpen";
+            this.buttonOpen.Size = new System.Drawing.Size(125, 24);
+            this.buttonOpen.TabIndex = 14;
+            this.buttonOpen.Text = "Open Folder";
+            this.buttonOpen.UseVisualStyleBackColor = true;
+            this.buttonOpen.Click += new System.EventHandler(this.buttonOpen_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(979, 846);
+            this.Controls.Add(this.buttonOpen);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.richTextBox3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.buttonMakeNew);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.listBoxRoms);
+            this.Controls.Add(this.buttonAddBios);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "CB Rom Wizzard";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button buttonAddBios;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox listBoxRoms;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button buttonMakeNew;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.RichTextBox richTextBox3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button buttonOpen;
+    }
+}
---
+++ cbrom/Form1.cs
@@ -0,0 +1,255 @@
+﻿using System;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace cbrom
+{
+    public partial class Form1 : Form
+    {
+
+        public string originalFile = string.Empty;
+        public string [] romMicroCodeFiles = null;
+        public static string currentFolder = Directory.GetCurrentDirectory();
+        public static string currentFile = string.Empty;
+        const string romReadExec = @"Bin\intelmicrocodelist.exe";
+        const string newBiosFileName = "new-bios.bin";
+        const string romWriteExec = @"Bin\CBROM32_195.exe";
+        public string newBiosFldr = currentFolder + @"\New Bios";
+
+        public Form1()
+        {
+            InitializeComponent();
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                richTextBox3.Text += "Opened: " + openFileDialog1.FileName + "\n";
+                currentFile = openFileDialog1.FileName;
+
+                richTextBox1.Text = readRom(currentFile);               
+
+
+                //string cmdExec = "/c " + currentFolder + @"\" + romExec + @" " + currentFile;// + @" " + romOut;
+
+
+                //var p = new Process();                
+                //p.StartInfo.UseShellExecute = false; 
+                //p.StartInfo.RedirectStandardOutput = true;
+                //string eOut = null;
+                //p.ErrorDataReceived += new DataReceivedEventHandler((sender, e) =>
+                //{ eOut += e.Data; });
+                //p.StartInfo.RedirectStandardError = true;
+                //p.StartInfo.RedirectStandardInput = true;
+
+                //p.StartInfo.FileName = currentFolder + @"\" + romExec;                
+                //p.StartInfo.Arguments = currentFile;
+
+                //p.Start();
+                //p.StandardInput.Write(p.StandardInput.NewLine);
+                //p.StandardInput.Write(p.StandardInput.NewLine);
+                //p.StandardInput.Write(p.StandardInput.NewLine);
+
+
+                //string output = p.StandardOutput.ReadToEnd();
+
+
+                //p.WaitForExit();
+
+            }
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            openFileDialog1.Title = "Browse Bios Roms files";
+            openFileDialog1.DefaultExt = "BIN";
+            openFileDialog1.Filter = "bin files (*.bin)|*.txt|rom files (*.rom)|*.rom|bios files (*.f*)|*.f*|All files (*.*)|*.*";
+            openFileDialog1.FileName = "*";
+
+            listBoxRoms.Items.Clear();
+
+
+            //foreach (var item in Directory.GetFiles(@"C:\Users\Dan\OneDrive\Documents\repos\cbrom\cbrom\bin\Debug\netcoreapp3.1\microcode", "*.bin"))
+            foreach (var item in Directory.GetFiles(currentFolder + @"\microcode", "*.bin"))
+            {
+                
+                listBoxRoms.Items.Add(item.ToString().Replace(currentFolder+@"\microcode\",""));
+            }
+        }
+
+        static public void getRoms(ref string[] roms)
+        {
+            int i = 0;
+            foreach (var item in Directory.GetFiles(@".\microcode", "*.bin"))
+            {                
+                roms[i] = item.ToString();
+                i++;
+            }
+        }
+
+        private void listBoxRoms_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void listBoxRoms_DoubleClick(object sender, EventArgs e)
+        {
+            if (listBoxRoms.Items.Count > 0 & listBoxRoms.SelectedIndex >=0)
+            {
+                
+                if(listBox1.Items.Count > 0)
+                {
+                    MessageBox.Show("Only one rom only can be used.", "Selection Error.", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                else
+                { 
+                listBox1.Items.Add(listBoxRoms.Items[listBoxRoms.SelectedIndex].ToString());
+                listBoxRoms.Items.Remove(listBoxRoms.SelectedItem.ToString());
+                }
+            }
+        }
+
+        private void listBox1_DoubleClick(object sender, EventArgs e)
+        {
+            if (listBox1.Items.Count > 0 & listBox1.SelectedIndex >= 0)
+            {
+                listBoxRoms.Items.Add(listBox1.Items[listBox1.SelectedIndex].ToString());
+                listBox1.Items.Remove(listBox1.SelectedItem.ToString());
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            
+            
+            if (listBox1.Items.Count != 0 & listBox1.Items.Count >= 0)
+            {
+                string ncpuRom = currentFolder + @"\NCPUCODE.BIN";
+
+
+                System.IO.File.Copy(currentFile, currentFolder + @"\" + newBiosFileName,true);
+                //MessageBox.Show(currentFolder + @"\microcode\" + listBox1.Items[0]);
+                System.IO.File.Copy(currentFolder + @"\microcode\" + listBox1.Items[0], ncpuRom,true);
+                var attr = File.GetAttributes(ncpuRom);
+                
+                attr = attr | FileAttributes.ReadOnly;  
+                File.SetAttributes(ncpuRom, attr);
+
+                //ProcessStartInfo p = new ProcessStartInfo(@"Bin\CBROM32_195.exe", newBiosFileName +@" /d");
+                //Process process = Process.Start(p);
+
+                richTextBox3.Text += "\n\n" + RomWrite();
+
+                attr = attr & ~FileAttributes.ReadOnly;
+                File.SetAttributes(ncpuRom, attr);                             
+
+                richTextBox2.Text = readRom(currentFolder + @"\" + newBiosFileName);
+
+                richTextBox3.AppendText("\n\n==============================\n" + "Lines Rom1: " + richTextBox1.Lines.Count().ToString() +
+                    "\nLines Rom2: " + richTextBox2.Lines.Count().ToString() + "\n==============================\n");
+                
+                                
+                
+                try
+                {                    
+                    System.IO.File.Move(currentFolder + @"\" + newBiosFileName, newBiosFldr + @"\" + newBiosFileName, true);
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("Unable to move new new bios file to destination.","Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    
+                }
+                
+
+                richTextBox3.Focus();
+                richTextBox3.ScrollToCaret();
+            }
+            // Make a new copy of current bios
+            // copy file needed to add to biod rom (NCPUCODE.BIN)
+            // make NCPUCODE.BIN  readonly
+            // execute cbrom195 "Your Bios".bin /d
+        }
+
+        private static string RomWrite()
+        {
+
+            var ProcessMicrocodeList = new Process();
+            ProcessMicrocodeList.StartInfo.UseShellExecute = false;
+            
+            string eOut = null;
+            ProcessMicrocodeList.ErrorDataReceived += new DataReceivedEventHandler((sender, e) =>
+            { eOut += e.Data; });
+            ProcessMicrocodeList.StartInfo.RedirectStandardOutput = true;
+            ProcessMicrocodeList.StartInfo.RedirectStandardError = true;
+            ProcessMicrocodeList.StartInfo.RedirectStandardInput = true;
+
+            ProcessMicrocodeList.StartInfo.FileName = currentFolder + @"\" + romWriteExec;
+            ProcessMicrocodeList.StartInfo.Arguments = newBiosFileName + " /nc_cpucode ncpucode.bin";
+
+            ProcessMicrocodeList.Start();            
+
+            string output = "\n\n"+ProcessMicrocodeList.StandardOutput.ReadToEnd();
+
+            ProcessMicrocodeList.WaitForExit();
+            ProcessMicrocodeList.Close();
+            ProcessMicrocodeList.Dispose();
+
+            return output;
+        }
+
+        private static string readRom(string fileName)
+        {          
+
+            var ProcessMicrocodeList = new Process();
+            ProcessMicrocodeList.StartInfo.UseShellExecute = false;
+            
+            string eOut = null;
+            ProcessMicrocodeList.ErrorDataReceived += new DataReceivedEventHandler((sender, e) =>
+            { eOut += e.Data; });
+            ProcessMicrocodeList.StartInfo.RedirectStandardOutput = true;
+            ProcessMicrocodeList.StartInfo.RedirectStandardError = true;
+            ProcessMicrocodeList.StartInfo.RedirectStandardInput = true;
+
+            ProcessMicrocodeList.StartInfo.FileName = currentFolder + @"\" + romReadExec;       
+            ProcessMicrocodeList.StartInfo.Arguments = fileName;
+
+            ProcessMicrocodeList.Start();
+            ProcessMicrocodeList.StandardInput.Write(ProcessMicrocodeList.StandardInput.NewLine);
+            ProcessMicrocodeList.StandardInput.Write(ProcessMicrocodeList.StandardInput.NewLine);
+            ProcessMicrocodeList.StandardInput.Write(ProcessMicrocodeList.StandardInput.NewLine);
+
+
+            string output = ProcessMicrocodeList.StandardOutput.ReadToEnd();
+
+
+            ProcessMicrocodeList.WaitForExit();
+            ProcessMicrocodeList.Close();
+            ProcessMicrocodeList.Dispose();
+
+            return output;
+        }
+
+        private void buttonOpen_Click(object sender, EventArgs e)
+        {
+            Process.Start("explorer.exe", newBiosFldr);            
+        }
+    }
+}
